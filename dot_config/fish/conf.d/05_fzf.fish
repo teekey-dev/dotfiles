0353c4d549c8ec47ddfc3952bb46a8e4ac2bf8ca
Teekey <hegrecomm@gmail.com>
2026-06-05 13:59:35 +0900

    feat: update fzf auto complete

--- dot_config/fish/conf.d/05_fzf.fish
+++ dot_config/fish/conf.d/05_fzf.fish
@@ -1,1 +1,10 @@
 fzf --fish | source
+
+# Detailed process table for `kill <Shift-Tab>` (port of fzf's bash/zsh _fzf_complete_kill)
+function _fzf_complete_kill
+    command ps -eo user,pid,ppid,start,time,command | _fzf_complete --multi --header-lines=1 --no-preview --wrap -- $argv
+end
+
+function _fzf_complete_kill_post
+    awk '{print $2}'
+end
